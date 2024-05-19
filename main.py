baea78317dda3a0a9575faeaaebe8bd6880f865e
пчел <ersovavaleria215@gmail.com>
2024-05-19 16:51:36 +1000

    добавила текст и проверку

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 from PyQt5.QtCore import Qt
+from window import TypingFilter
+from Text import DisplayText
 from Keyboard import Keyboard
-from Input import Input
-
+from Englelvls import english_levels_2, english_levels_1
 
 class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("QuickKeys")
+        self.setWindowTitle("Typing Trainer")
+        self.setFixedSize(600, 400)
+
+        self.display_text = DisplayText(self)
+        self.typing_filter = TypingFilter(self.display_text, self)
+        self.typing_filter.set_level_text(english_levels_1[0])
+
+
         keyboard = Keyboard()
-        input = Input() 
-        main_layout = QGridLayout()
-        main_layout.addWidget(input, 0, 0)
-        main_layout.addWidget(keyboard, 1, 0, alignment=Qt.AlignCenter)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.display_text)
+        layout.addWidget(self.typing_filter)
+        layout.addWidget(keyboard)
+
         container = QWidget()
-        container.setLayout(main_layout)
+        container.setLayout(layout)
         self.setCentralWidget(container)
-
-
-    
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
