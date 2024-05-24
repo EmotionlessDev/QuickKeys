33e3379824a1158a6832fb9d3a90c64f1354c6d2
EmotionlessDev <emotionlesscode@gmail.com>
2024-05-25 00:00:51 +1000

    add cpm

--- Input.py
+++ Input.py
@@ -13,6 +13,7 @@ class Input(QLineEdit):
         self.textChanged.connect(self.checkInput)
         self.current_word_start = 0
         self.correct_words = 0
+        self.correct_letters = 0
 
     def checkInput(self):
         input_text = self.text()
@@ -20,9 +21,9 @@ class Input(QLineEdit):
             word = input_text.strip()
             if word:
                 cur_word = self.textfield.original_text.split(" ")[0]  # Get the first word
-                print(cur_word)
                 if word == cur_word:
                     self.correct_words += 1
+                    self.correct_letters += len(cur_word)
                     self.score_label.setText(f"Correct Words: {self.correct_words}")
                 # Remove the first word and update text
                 remaining_text = ' '.join(self.textfield.original_text.split(' ')[1:])
@@ -30,8 +31,15 @@ class Input(QLineEdit):
                 self.textfield.updateText(0)
             self.setText("")
 
+    def getCorrectWords(self):
+        return self.correct_words
+
+    def getCorrectLetters(self):
+        return self.correct_letters
+
     def reset(self):
         self.correct_words = 0
+        self.correct_letters = 0
         self.current_word_start = 0
         self.score_label.setText(f"Correct Words: {self.correct_words}")
         self.textfield.genText()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QGridLayout, QLabel, QPushButton
-from PyQt5.QtCore import Qt, QTimer, QTime 
+from PyQt5.QtCore import Qt, QTimer, QTime, ws
 from window import TypingFilter
 from Text import DisplayText
 from Keyboard import Keyboard
@@ -8,7 +8,9 @@ from Englelvls import english_levels_2, english_levels_1
 from Input import Input
 from Textfield import Textfield
 
+
 class MainWindow(QMainWindow):
+
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Typing Trainer")
@@ -18,7 +20,7 @@ class MainWindow(QMainWindow):
         score_label = QLabel()
         self.reload = QPushButton("Restart")
         self.reload.clicked.connect(self.reset)
-        self.input = Input(textField, score_label) 
+        self.input = Input(textField, score_label)
         self.input.textEdited.connect(keyboard.highlightButton)
 
         self.timer = QTimer()  # Основной таймер
@@ -30,24 +32,43 @@ class MainWindow(QMainWindow):
         self.input_started = False
         self.timer_label = QLabel()
         self.current_time = QTime(0, 1)
+        self.timer_label.setText(self.current_time.toString("mm:ss"))
 
         self.input.textChanged.connect(self.startTimer)
         self.timer.timeout.connect(self.endTimer)
         self.child_timer.timeout.connect(self.updateChildTimer)
 
+        self.speed_label = QLabel()
+        self.speed_label.setText("0 CPM")
+        self.input.textChanged.connect(self.updateSpeedLabel)
+
         main_layout = QGridLayout()
-        main_layout.addWidget(score_label, 0, 0)
+        main_layout.addWidget(self.speed_label, 0, 0)
+        main_layout.addWidget(score_label, 0, 1)
         main_layout.addWidget(self.reload, 0, 2)
-        main_layout.addWidget(self.timer_label, 0, 1)
-        main_layout.addWidget(textField, 1, 0, 1, 3)
-        main_layout.addWidget(self.input, 2, 0, 1, 3)
+        main_layout.addWidget(self.timer_label, 0, 3)
+        main_layout.addWidget(textField, 1, 0, 1, 4)
+        main_layout.addWidget(self.input, 2, 0, 1, 4)
         main_layout.addWidget(keyboard, 3, 0, alignment=Qt.AlignCenter)
         container = QWidget()
         container.setLayout(main_layout)
         self.setCentralWidget(container)
 
+    def updateSpeedLabel(self):
+        cur_t = self.current_time.minute() * 60 + self.current_time.second() 
+        if cur_t != 0:
+            cur_speed = self.input.getCorrectLetters() * 60 / cur_t
+        else:
+            cur_speed = 0
+        self.speed_label.setText(str(cur_speed) + " CPM")
+
     def reset(self):
-       self.input.reset() 
+        self.input.reset()
+        self.timer.stop()
+        self.child_timer.stop()
+        self.current_time = QTime(0, 1)
+        self.input_started = False
+        self.timer_label.setText(self.current_time.toString("mm:ss"))
 
     def startTimer(self):
         # Если ввод еще не начался, запускаем таймер
@@ -57,17 +78,18 @@ class MainWindow(QMainWindow):
             self.child_timer.start()  # Запускаем дочерний таймер
 
     def endTimer(self):
-        # вывести статистику & reset
         self.timer.stop()
         self.child_timer.stop()  # Останавливаем дочерний таймер
         self.input_started = False
         self.updateChildTimer()
         print("Time's up!")
+        
 
     def updateChildTimer(self):
         # Обновляем текущее время и выводим его на QLabel
         self.current_time = self.current_time.addSecs(-1)
-        self.timer_label.setText(self.current_time.toString("mm:ss")) 
+        self.timer_label.setText(self.current_time.toString("mm:ss"))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
